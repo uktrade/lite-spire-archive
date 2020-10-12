711f6076ad503db305beadbd1f7488f63c04fc8c
Kerin Cosford <cosford@gmail.com>
2020-10-12 13:22:26 +0100

    Update DDL files from sqldeveloper output

--- spire-oracle/sql/TABLES/TRACE_WUA_CONTROL.sql
+++ spire-oracle/sql/TABLES/TRACE_WUA_CONTROL.sql
@@ -2,8 +2,7 @@
 --  DDL for Table TRACE_WUA_CONTROL
 --------------------------------------------------------
 
-  CREATE TABLE "FOXMGR"."TRACE_WUA_CONTROL" 
+  CREATE TABLE "SPIREMGR"."TRACE_WUA_CONTROL" 
    (	"WUA_ID" NUMBER, 
-	"END_DATETIME" DATE, 
-	"TRACE_NAME" VARCHAR2(100 BYTE)
+	"END_DATETIME" DATE
    ) ;
